40198972204f74888d498f8ec8d1ede7a096bebf
svenschilling <sven.schilling@gmail.com>
2021-10-24 23:57:25 +0200

    update

--- main.py
+++ main.py
@@ -2,9 +2,14 @@ import numpy,matplotlib.pyplot
 
 class neuralNetwork:
 
-    def __init__(self):
-        pass
-
+    def __init__(self,inputNodes,hiddenNodes,outputNodes,learningRate):
+        # nodes
+        self.inodes = inputNodes
+        self.hnodes = hiddenNodes
+        self.onodes = outputNodes
+        # learningrate
+        self.lr = learningRate
+        
     def train(self):
         pass
 
